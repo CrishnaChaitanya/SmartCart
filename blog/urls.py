3b95363179ebde5a3b4cf785a0595ba56d1d84c6
CrishnaChaitanya <121910302022@gitam.in>
2021-01-31 19:54:22 +0530

    changed

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
+
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('contactUs/', views.contactUs, name='blog-contactus'),
@@ -14,8 +11,5 @@ urlpatterns = [
     path('shop/', views.shop, name='blog-shop'),
     path('logout/', views.loginPage, name="logout"),
     path('product/', views.product, name='blog-product'),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+
 ]
