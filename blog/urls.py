7f7021735d77b560e2fa6d800fc7d4bb9f6b9217
CrishnaChaitanya <121910302022@gitam.in>
2021-01-31 19:13:29 +0530

    hero

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('contactUs/', views.contactUs, name='blog-contactus'),
@@ -9,5 +11,9 @@ urlpatterns = [
     path('register/', views.register, name='blog-register'),
     path('shop/', views.shop, name='blog-shop'),
     path('logout/', views.loginPage, name="logout"),
-    path('product/', views.product, name='blog-product')
+    path('product/', views.product, name='blog-product'),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
--- danjo_project/settings.py
+++ danjo_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$z6wzo3@nv&!2)+9l$h3&_cvp_d*rz(v9%h)n#+=pn0ifh7q3a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
